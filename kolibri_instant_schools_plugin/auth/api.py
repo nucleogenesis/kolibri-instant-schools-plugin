957bfb24054c85d8a2b66718a616b2ac9fb161b8
Jacob Pierce <w.jacobpierce@gmail.com>
2022-10-19 12:35:56 -0700

    setup task and jobs to send data if proper envvar is set, retries for up to 7 days before anonymizing the data

--- kolibri_instant_schools_plugin/auth/api.py
+++ kolibri_instant_schools_plugin/auth/api.py
@@ -1,27 +1,54 @@
-from django.conf import settings
-from django.contrib.auth import update_session_auth_hash
+import logging
+import os
+
+import requests
 from django.db import transaction
 from django.utils.translation import ugettext as _
-from rest_framework import filters, permissions, status, viewsets, serializers
-from rest_framework.response import Response
-from rest_framework.decorators import action
-from kolibri.core.auth.models import Facility, FacilityUser
-from kolibri.core.auth.api import SignUpViewSet, FacilityUserViewSet
+from kolibri.core.auth.api import FacilityUserViewSet
+from kolibri.core.auth.api import SignUpViewSet
+from kolibri.core.auth.models import FacilityUser
 from kolibri.core.auth.serializers import FacilityUserSerializer
+from kolibri.core.tasks.decorators import register_task
+from rest_framework import serializers
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from .mapping import get_usernames, create_new_username, normalize_phone_number, hash_phone
-from ..models import PasswordResetToken, PhoneHashToUsernameMapping
-from ..smpp.utils import send_password_reset_link, SMSConnectionError
+from ..models import PasswordResetToken
+from ..models import PhoneHashToUsernameMapping
+from ..smpp.utils import send_password_reset_link
+from ..smpp.utils import SMSConnectionError
+from .mapping import create_new_username
+from .mapping import get_usernames
+from .mapping import hash_phone
+from .mapping import normalize_phone_number
 
 
 class PhoneNumberSignupSerializer(FacilityUserSerializer):
-
     def validate_username(self, value):
         if FacilityUser.objects.filter(username__iexact=value).exists():
-            raise serializers.ValidationError(_('An account already exists for this phone number. To add a new profile ' +
-                                                'under this account, you must first login. If you have forgotten your ' +
-                                                'password, you can reset it using the link on the login page.'))
+            raise serializers.ValidationError(
+                _(
+                    "An account already exists for this phone number. To add a new profile "
+                    + "under this account, you must first login. If you have forgotten your "
+                    + "password, you can reset it using the link on the login page."
+                )
+            )
         return value
+
+
+@register_task()
+def send_user_data_to_opco(raw_user_data, **kwargs):
+    url = os.environ.get("POST_USER_URL", None)
+    if url:
+        requests.post(url, data=raw_user_data).raise_for_status()
+    else:
+        logging.warn(
+            "No URL set to post data. If you are expecting to send data to the OpCos,\
+                      you should set POST_USER_URL and restart the server. After a week \
+                      failures the data will be anonymized and it will be lost permanently"
+        )
 
 
 class PhoneNumberSignUpViewSet(SignUpViewSet):
@@ -31,6 +58,15 @@ class PhoneNumberSignUpViewSet(SignUpViewSet):
     def extract_request_data(self, request):
         data = super(PhoneNumberSignUpViewSet, self).extract_request_data(request)
 
+        # Copy the data so we can create our own job-specific version of it
+        job_data = data.copy()
+        job_data["failures_count"] = 0
+        del job_data["password"]
+
+        # data['username'] is what the user put into the Phone Number field on sign up
+        # we're about to hash it, so store it in job_data before hand
+        job_data["phone"] = data["username"]
+
         # if there are already users for this number, use one, to trigger a validation error, else create a new one
         usernames = get_usernames(hash_phone(data["username"]))
         if usernames:
@@ -38,11 +74,13 @@ class PhoneNumberSignUpViewSet(SignUpViewSet):
         else:
             data["username"] = create_new_username(data["username"])
 
+        # job_data should now have no password, a 0 failures counter and the raw phone number in addition
+        # to the normal fields included there
+        send_user_data_to_opco.enqueue(job_data)
         return data
 
 
 class PasswordResetTokenViewset(viewsets.ViewSet):
-
     def create(self, request):
         """
         Initiate the password reset process by generating a token for a phone number, and sending a link via SMS.
@@ -56,24 +94,31 @@ class PasswordResetTokenViewset(viewsets.ViewSet):
         """
 
         # extract the phone number from the request
-        phone = normalize_phone_number(request.data.get('phone', ''))
+        phone = normalize_phone_number(request.data.get("phone", ""))
 
         # ensure we have an account for this phone number
         if not get_usernames(phone):
-            return Response(_("No account found for this phone number."), status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                _("No account found for this phone number."),
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         # generate a new token for the phone number
         token = PasswordResetToken.generate_new_token(phone=phone)
 
         # determine base URL from the scheme/host in this request, so we send people back to a server they can access
-        baseurl = "{scheme}://{host}".format(scheme=request.scheme, host=request.get_host())
+        baseurl = "{scheme}://{host}".format(
+            scheme=request.scheme, host=request.get_host()
+        )
 
         # send the password reset URL to the phone number via SMS
         try:
             send_password_reset_link(phone, token.token, baseurl)
         except SMSConnectionError:
-            return Response(_("Error sending SMS message; please try again later."),
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(
+                _("Error sending SMS message; please try again later."),
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
         # return a 201 to indicate having successfully sent the reset URL
         return Response("", status=status.HTTP_201_CREATED)
@@ -88,7 +133,7 @@ class PasswordResetTokenViewset(viewsets.ViewSet):
                 If token exists and is valid, returns status 200.
                 Otherwise, returns status 400.
         """
-        phone = normalize_phone_number(request.query_params.get('phone', ''))
+        phone = normalize_phone_number(request.query_params.get("phone", ""))
 
         try:
             obj = PasswordResetToken.objects.get(token=pk, phone=phone)
@@ -101,15 +146,18 @@ class PasswordResetTokenViewset(viewsets.ViewSet):
 
 
 class FacilityUserProfileViewset(FacilityUserViewSet):
-
     def set_password_if_needed(self, instance, serializer):
         with transaction.atomic():
-            if serializer.validated_data.get('password', ''):
+            if serializer.validated_data.get("password", ""):
                 # update the password for all accounts associated with this password
-                hashed_phone = PhoneHashToUsernameMapping.objects.get(username=instance.username).hash
-                set_password_for_hashed_phone(hashed_phone, serializer.validated_data['password'])
+                hashed_phone = PhoneHashToUsernameMapping.objects.get(
+                    username=instance.username
+                ).hash
+                set_password_for_hashed_phone(
+                    hashed_phone, serializer.validated_data["password"]
+                )
                 # explicitly update password for this user to avoid sign out
-                instance.set_password(serializer.validated_data['password'])
+                instance.set_password(serializer.validated_data["password"])
                 instance.save()
 
 
@@ -125,7 +173,6 @@ def set_password_for_hashed_phone(hashed_phone, password):
 
 
 class PasswordChangeViewset(viewsets.ViewSet):
-
     def create(self, request):
         """
         Change the password for the full set of FacilityUser profiles associated with a phone number.
@@ -139,9 +186,9 @@ class PasswordChangeViewset(viewsets.ViewSet):
         """
 
         # extract the token, password, and phone number from the request data
-        token = request.data['token']
-        password = request.data['password']
-        phone = request.data['phone']
+        token = request.data["token"]
+        password = request.data["password"]
+        phone = request.data["phone"]
 
         # try to find a reset token matching the phone number and token, otherwise error out
         try:
@@ -161,7 +208,6 @@ class PasswordChangeViewset(viewsets.ViewSet):
 
 
 class PhoneAccountProfileViewset(viewsets.ViewSet):
-
     def create(self, request):
         """
         Create a new "profile" (FacilityUser) for a given phone number.
@@ -174,12 +220,14 @@ class PhoneAccountProfileViewset(viewsets.ViewSet):
         """
 
         # extract the data from the request
-        phone = normalize_phone_number(request.data['phone'])
-        password = request.data['password']
-        full_name = request.data['full_name']
+        phone = normalize_phone_number(request.data["phone"])
+        password = request.data["password"]
+        full_name = request.data["full_name"]
 
         # get the list of existing profiles (users) for the phone number
-        users = FacilityUser.objects.filter(username__in=get_usernames(hash_phone(phone)))
+        users = FacilityUser.objects.filter(
+            username__in=get_usernames(hash_phone(phone))
+        )
 
         if not users:
             return Response("", status=status.HTTP_401_UNAUTHORIZED)
@@ -192,7 +240,9 @@ class PhoneAccountProfileViewset(viewsets.ViewSet):
         username = create_new_username(phone)
 
         # create the new FacilityUser for the profile
-        user = FacilityUser(username=username, full_name=full_name, facility=users[0].facility)
+        user = FacilityUser(
+            username=username, full_name=full_name, facility=users[0].facility
+        )
         user.set_password(password)
         user.save()
 
@@ -211,11 +261,13 @@ class PhoneAccountProfileViewset(viewsets.ViewSet):
                 If password fails, returns status 401.
         """
         # extract the phone and password from the query params
-        phone = normalize_phone_number(request.data.get('phone', ''))
-        password = request.data.get('password', '')
+        phone = normalize_phone_number(request.data.get("phone", ""))
+        password = request.data.get("password", "")
 
         # get all user profiles associated with the phone number
-        users = FacilityUser.objects.filter(username__in=get_usernames(hash_phone(phone)))
+        users = FacilityUser.objects.filter(
+            username__in=get_usernames(hash_phone(phone))
+        )
 
         # return a 404 if there are no users for this phone number
         if not users:
@@ -225,4 +277,10 @@ class PhoneAccountProfileViewset(viewsets.ViewSet):
         if not users[0].check_password(password):
             return Response("", status=status.HTTP_401_UNAUTHORIZED)
 
-        return Response([{"username": user.username, "full_name": user.full_name} for user in users], status=status.HTTP_200_OK)
+        return Response(
+            [
+                {"username": user.username, "full_name": user.full_name}
+                for user in users
+            ],
+            status=status.HTTP_200_OK,
+        )
